65f4bce9e87bccc69485ed7a88c24d30a4a3dbe8
alastairruhm <alastairruhm@gmail.com>
2017-09-01 10:07:04 +0800

    benchmark test

---
+++ twoSum/main_benchmark_test.go
@@ -0,0 +1,16 @@
+package twoSum
+
+import "testing"
+
+var (
+	nums   = []int{4, 6, 7, 8, 9, 1, 3, 11}
+	target = 12
+)
+
+func benchmarkAddTwo(AddTwo func([]int, int) []int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		AddTwo(nums, target)
+	}
+}
+
+func BenchmarkAddTwo_1(b *testing.B) { benchmarkAddTwo(twoSum, b) }
