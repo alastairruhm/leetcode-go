c2b689b8adb4bd57cf4cf27798713838d119553c
alastairruhm <alastairruhm@gmail.com>
2017-09-07 14:19:46 +0800

    test: boundry condition

--- twoSum/main_test.go
+++ twoSum/main_test.go
@@ -22,6 +22,7 @@ func (s *TestSuite) TestAddTwo(c *C) {
 	}{
 		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
 		{[]int{3, 2, 4}, 6, []int{1, 2}},
+		{[]int{1, 2, 3}, 9, []int{0, 0}},
 	}
 
 	for _, tt := range addTwoTests {
