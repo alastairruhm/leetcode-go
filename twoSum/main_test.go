983ff10d15e2c52366eeb8ab1afad1e3a1302d84
alastairruhm <alastairruhm@gmail.com>
2017-09-07 13:01:26 +0800

    test: test with gocheck lib

--- twoSum/main_test.go
+++ twoSum/main_test.go
@@ -3,7 +3,33 @@ package twoSum
 import (
 	"reflect"
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type TestSuite struct{}
+
+var _ = Suite(&TestSuite{})
+
+func (s *TestSuite) TestAddTwo(c *C) {
+
+	var addTwoTests = []struct {
+		nums     []int
+		target   int
+		expected []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+	}
+
+	for _, tt := range addTwoTests {
+		result := twoSum(tt.nums, tt.target)
+		c.Check(result, DeepEquals, tt.expected)
+	}
+}
 
 func TestAddTwo(t *testing.T) {
 	var addTwoTests = []struct {
@@ -20,5 +46,4 @@ func TestAddTwo(t *testing.T) {
 			t.Fatalf("should be %v, but is:%v\n", tt.expected, result)
 		}
 	}
-
 }
