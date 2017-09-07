b0013829720d44ac5d6e505c92e3de0400f05ed7
alastairruhm <alastairruhm@gmail.com>
2017-09-07 15:15:23 +0800

    refactor: package scoped variables

--- reverseInteger/main_test.go
+++ reverseInteger/main_test.go
@@ -6,16 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var cases = []struct {
+	input    int
+	expected int
+}{
+	{123, 321},
+	{-123, -321},
+	{1000000003, 0},
+	{-467, -764},
+}
+
 func TestReverse(t *testing.T) {
 	assert := assert.New(t)
-	var cases = []struct {
-		input    int
-		expected int
-	}{
-		{123, 321},
-		{-123, -321},
-		{1000000003, 0},
-	}
 
 	for _, tt := range cases {
 		assert.Equal(reverse(tt.input), tt.expected)
@@ -25,14 +27,6 @@ func TestReverse(t *testing.T) {
 
 func TestReverseWithGoroutine(t *testing.T) {
 	assert := assert.New(t)
-	var cases = []struct {
-		input    int
-		expected int
-	}{
-		{123, 321},
-		{-123, -321},
-		{1000000003, 0},
-	}
 
 	for _, tt := range cases {
 		assert.Equal(reverseWithGoroutine(tt.input), tt.expected)
@@ -41,14 +35,6 @@ func TestReverseWithGoroutine(t *testing.T) {
 
 func TestReverseIn3msSolution(t *testing.T) {
 	assert := assert.New(t)
-	var cases = []struct {
-		input    int
-		expected int
-	}{
-		{123, 321},
-		{-123, -321},
-		{1000000003, 0},
-	}
 
 	for _, tt := range cases {
 		assert.Equal(reverseIn3msSolution(tt.input), tt.expected)
