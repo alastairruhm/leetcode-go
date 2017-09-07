04ea1c76a99a866bc86df9aa28f920a7ae22010d
alastairruhm <alastairruhm@gmail.com>
2017-09-07 14:08:52 +0800

    chore: try goveralls

--- twoSum/main_test.go
+++ twoSum/main_test.go
@@ -1,7 +1,6 @@
 package twoSum
 
 import (
-	"reflect"
 	"testing"
 
 	. "gopkg.in/check.v1"
@@ -28,22 +27,5 @@ func (s *TestSuite) TestAddTwo(c *C) {
 	for _, tt := range addTwoTests {
 		result := twoSum(tt.nums, tt.target)
 		c.Check(result, DeepEquals, tt.expected)
-	}
-}
-
-func TestAddTwo(t *testing.T) {
-	var addTwoTests = []struct {
-		nums     []int
-		target   int
-		expected []int
-	}{
-		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
-		{[]int{3, 2, 4}, 6, []int{1, 2}},
-	}
-
-	for _, tt := range addTwoTests {
-		if result := twoSum(tt.nums, tt.target); !reflect.DeepEqual(result, tt.expected) {
-			t.Fatalf("should be %v, but is:%v\n", tt.expected, result)
-		}
 	}
 }
