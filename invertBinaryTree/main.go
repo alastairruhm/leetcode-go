53b31aea18d8b5073ebd262ea581d1eebbdcf0ff
alastairruhm <alastairruhm@gmail.com>
2018-04-10 08:50:12 +0800

    invert binary tree

---
+++ invertBinaryTree/main.go
@@ -0,0 +1,28 @@
+package invertBinaryTree
+
+/**
+* Definition for a binary tree node.
+* type TreeNode struct {
+*     Val int
+*     Left *TreeNode
+*     Right *TreeNode
+* }
+ */
+
+// TreeNode definition of binary tree
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = invertTree(root.Left)
+	root.Right = invertTree(root.Right)
+
+	root.Left, root.Right = root.Right, root.Left
+	return root
+}
